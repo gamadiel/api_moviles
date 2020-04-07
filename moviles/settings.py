8a258124078d031b78d493f5f39225ac05f20b34
gamadiel <153204@ids.upchiapas.edu.mx>
2020-04-07 01:04:53 -0500

    parentesis database_url

--- moviles/settings.py
+++ moviles/settings.py
@@ -104,9 +104,9 @@ import dj_database_url
 from decouple import config
 
 DATABASES ={
-    'default' : dj_database_url.config{
+    'default' : dj_database_url.config( 
         default = config('DATABASE_URL')
-    }
+    )
 
 }
 
